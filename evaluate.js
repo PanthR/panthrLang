c5b292b8833227d91870594edc402e5a35992153
Haris Skiadas <cskiadas@gmail.com>
2016-05-06 14:20:15 -0400

    Rewrite the evaluation process to have a more functional feel. Close #11

--- evaluate.js
+++ evaluate.js
@@ -8,111 +8,105 @@ define(function(require) {
    Value = require('./value');
 
    function Evaluate() {
-      this.reset();
-      // Users overwrite this
-      this.emit = function(value, node) {};
+      this.global = Frame.newGlobal();
    }
 
    Evaluate.prototype = {
-      reset: function() {
-         this.frame = Frame.newGlobal();
-      },
-      // "runs" a certain node to completion.
-      // Emits the resulting value
       eval: function(node) {
-         var val;
-
-         val = this.run(node);
-         this.emit(val);
-
-         return val;
-      },
-      run: function run(node) {
-         switch (node.name) {
-         case 'number': return Value.make_numeric(node.args[0]);
-         case 'range': return this.run(node.args[0]); // FIXME
-         case 'arithop':
-            return do_arith(node.args[0],
-                            this.run(node.args[1]),
-                            this.run(node.args[2]));
-         case 'var':
-            return this.lookup(node.args[0]);
-         case 'assign':
-            return this.assign(node.args[0].args[0], this.run(node.args[1]));
-         case 'assign_inherit':
-            return this.assignInherit(node.args[0].args[0], this.run(node.args[1]));
-         case 'fun_def':
-            return Value.make_closure(node, this.frame);
-         case 'expr_seq':
-            return this.eval_seq(node.args[0]);
-         case 'fun_call':
-            return this.eval_call(this.run(node.args[0]),
-                                  node.args[1].map(this.run.bind(this)));
-         default:
-            throw new Error('Unknown node: ' + node.name);
-         }
-      },
-      lookup: function lookup(symbol) {
-         var val;
-
-         val = this.frame.lookup(symbol);
-         if (val === null) {
-            throw new Error("Unknown property: ", symbol);
-         }
-         return val;
-      },
-      // Assigns value in the current frame (possibly shadowing existing value)
-      assign: function assign(symbol, value) {
-         this.frame.store(symbol, value);
-
-         return value;
-      },
-      // Assigns value to whichever frame in the inheritance chain the
-      // value is defined. If the value is not defined in a previous
-      // frame, it will be created as a global value.
-      // TODO: What about protecting functions like "c"?
-      assignInherit: function(symbol, value) {
-         var frame = this.frame;
-
-         while (!frame.hasOwnProperty(symbol) &&
-                frame.getParent() !== null) {
-            frame = frame.getParent();
-         }
-         frame.store(symbol, value);
-
-         return value;
-      },
-      eval_seq: function eval_seq(exprs) {
-         var i, val;
-
-         for (i = 0; i < exprs.length; i += 1) {
-            val = this.run(exprs[i]);
-         }
-         return val;
-      },
-      // TODO: This all should be done cleaner
-      eval_call: function eval_call(clos, actuals) {
-         var formals, body, oldFrame, i, result;
-         if (clos.type !== 'closure') {
-            throw new Error('trying to call non-closure');
-         }
-         formals = clos.value.func.args[0];
-         body = clos.value.func.args[1];
-         if (formals.length !== actuals.length) {
-            throw new Error('function called with wrong number of arguments');
-         }
-
-         oldFrame = this.frame;
-         this.frame = clos.value.env.extend();
-         for (i = 0; i < formals.length; i += 1) {
-            this.frame.store(formals[i].args[0], actuals[i]);
-         }
-         result = this.run(body);
-         this.frame = oldFrame;
-
-         return result;
+         return evalInFrame(node, this.global);
       }
    };
+
+   // "runs" a certain node to completion.
+   // Emits the resulting value
+   function evalInFrame(node, frame) {
+      switch (node.name) {
+      case 'number': return Value.make_numeric(node.args[0]);
+      case 'range': return evalInFrame(node.args[0], frames); // FIXME
+      case 'arithop':
+         return do_arith(node.args[0],
+                         evalInFrame(node.args[1], frame),
+                         evalInFrame(node.args[2], frame));
+      case 'var':
+         return lookup(node.args[0], frame);
+      case 'assign':
+         return assign(node.args[0].args[0],
+                       evalInFrame(node.args[1], frame),
+                       frame);
+      case 'assign_inherit':
+         return assignInherit(node.args[0].args[0],
+                              evalInFrame(node.args[1], frame),
+                              frame);
+      case 'fun_def':
+         return Value.make_closure(node, frame);
+      case 'expr_seq':
+         return eval_seq(node.args[0], frame);
+      case 'fun_call':
+         return eval_call(evalInFrame(node.args[0], frame),
+                          node.args[1].map(function(node) {
+                             return evalInFrame(node, frame);
+                          }));
+      default:
+         throw new Error('Unknown node: ' + node.name);
+      }
+   }
+
+   function lookup(symbol, frame) {
+      var val;
+
+      val = frame.lookup(symbol);
+      if (val === null) {
+         throw new Error("Unknown property: ", symbol);
+      }
+      return val;
+   }
+   // Assigns value in the current frame (possibly shadowing existing value)
+   function assign(symbol, value, frame) {
+      frame.store(symbol, value);
+
+      return value;
+   }
+   // Assigns value to whichever frame in the inheritance chain the
+   // value is defined. If the value is not defined in a previous
+   // frame, it will be created as a global value.
+   // TODO: What about protecting functions like "c"?
+   function assignInherit(symbol, value, frame) {
+      while (!frame.hasOwnProperty(symbol) &&
+             frame.getParent() !== null) {
+         frame = frame.getParent();
+      }
+      frame.store(symbol, value);
+
+      return value;
+   }
+
+   function eval_seq(exprs, frame) {
+      var i, val;
+
+      for (i = 0; i < exprs.length; i += 1) {
+         val = evalInFrame(exprs[i], frame);
+      }
+      return val;
+   }
+   // TODO: This all should be done cleaner
+   function eval_call(clos, actuals) {
+      var formals, body, closExtFrame, i, result;
+      if (clos.type !== 'closure') {
+         throw new Error('trying to call non-closure');
+      }
+      formals = clos.value.func.args[0];
+      body = clos.value.func.args[1];
+      if (formals.length !== actuals.length) {
+         throw new Error('function called with wrong number of arguments');
+      }
+
+      closExtFrame = clos.value.env.extend();
+      for (i = 0; i < formals.length; i += 1) {
+         closExtFrame.store(formals[i].args[0], actuals[i]);
+      }
+
+      return evalInFrame(body, closExtFrame);
+   }
 
    function do_arith(op, v1, v2) {
       if (v1.type !== 'numeric' || v2.type !== 'numeric') {
--- panthrlang.js
+++ panthrlang.js
@@ -29,7 +29,11 @@ define(function(require) {
       eval: function(str) {
          var vals, ev;
 
+         // Currently each time eval is called it creates a new "environment".
+         // Global environment is not maintained across invocations of this
+         // eval.
          ev = new Evaluate();
+
          this.parse(str, function(nodes) {
             vals = nodes.map(function(node) { return ev.eval(node); });
          });
