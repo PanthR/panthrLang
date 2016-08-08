b38a276cfeb109234d539a6314bf0afb0b377c52
Haris Skiadas <cskiadas@gmail.com>
2016-08-08 12:12:13 -0400

    Add function comments, and refactor type specifications. Ref #33

--- panthrLang/resolver.js
+++ panthrLang/resolver.js
@@ -7,42 +7,105 @@ define(function(require) {
    Value = require('./value');
    Base = require('panthrbase/index');
 
-   knownTypes = {};  // conversions
-   checkers = {};
-
-   // TODO: Add standard types and conversions, checkers
-
+   /*
+    * Instantiates a resolver object. The object will then be loaded with
+    * parameters and rules using the instance methods.
+    *
+    * See `Resolver#resolve` for details.
+    * When `Resolver#resolve` is called, it is provided an "actuals" list
+    * for the function call to be evaluated. This list is then compared to
+    * the specifications in the `parameters` property and an initial
+    * processing is performed (e.g. vector->number and other conversions).
+    * This produces a new list, which is then transformed using the `rules` one
+    * at a time in the order in which they were inserted in the array.
+    */
    function Resolver() {
+      /*
+       * Array of the parameter properties (name/allowed types/required)
+       */
       this.parameters = [];
+      /*
+       * Array of default/dependent/normalize rules to be applied to the
+       * initially processed actuals list.
+       */
       this.rules = [];
    }
 
-   Resolver.addType = function(type, check) {
-      if (!knownTypes.hasOwnProperty(type)) {
-         knownTypes[type] = {};
-         checkers[type] = check;
+   /*
+    * Each type object has:
+    * - The type's name as the key
+    * - A `check` property containing the type's checker predicate
+    * - A `unwrap` property containng the type's unwrapper (Value->javascript)
+    * - A `conversions` property containing key-value pairs where each
+    *     key is a target type and the value is the appropriate conversion function.
+    */
+   Resolver.types = {};
+
+   // TODO: Add standard types and conversions, checkers
+
+
+   /*
+    * Adds a new type based on the string `type`. The predicate `check`
+    * can take any value and must return a boolean indicating if that value is
+    * of the given type.
+    *
+    *
+    */
+   Resolver.addType = function(type, check, unwrap) {
+      if (!Resolver.types.hasOwnProperty(type)) {
+         Resolver.types[type] = {
+            check: check,
+            unwrap: unwrap,
+            conversions: {}
+         };
       }
 
       return this;
    };
 
+   Resolver.hasType = function(type) {
+      return Resolver.types.hasOwnProperty(type);
+   }
+
+   /*
+    * Sets up a new type conversion based on `fun`, if one does not already exist.
+    * `fun` should expect as input a value of type `from` and return a value of type `to`.
+    */
    Resolver.addConversion = function(from, to, fun) {
-      if (!knownTypes.hasOwnProperty(to)) {
+      if (!Resolver.hasType(to)) {
          throw new Error('Asked to convert to unknown type: ' + to);
       }
-      if (!knownTypes.hasOwnProperty(from)) {
+      if (!Resolver.hasType(from)) {
          throw new Error('Asked to convert from unknown type: ' + from);
       }
-      if (knownTypes[from].hasOwnProperty(to)) {
+      if (Resolver.types[from].conversions.hasOwnProperty(to)) {
          throw new Error('Already know how to convert from ' + from + ' to ' + to);
       }
-      knownTypes[from][to] = fun;
+      Resolver.types[from].conversions[to] = fun;
 
       return this;
-   }
+   };
+
+   Resolver.getConversion = function(from, to) {
+      if (!Resolver.hasType(to)) {
+         throw new Error('Unknown type: ' + to);
+      }
+      if (!Resolver.hasType(from)) {
+         throw new Error('Unknown type: ' + from);
+      }
+      if (!Resolver.types[from].conversions.hasOwnProperty(to)) {
+         throw new Error('Unknown conversion from ' + from + ' to ' + to);
+      }
+
+      return Resolver.types[from].conversions[to];
+   };
 
    Resolver.prototype = {};
 
+   /*
+    * Adds a new parameter. `types` may be a string or an array of strings.
+    * `required` is a boolean defaulting to `false`.
+    */
    Resolver.prototype.addParameter = function(param, types, required) {
       required = required === true;
 
@@ -51,7 +114,7 @@ define(function(require) {
       }
       if (typeof types !== 'function') {
          types.forEach(function(type) {
-            if (!Resolver.knownTypes.hasOwnProperty(type)) {
+            if (!Resolver.hasType(type)) {
                throw new Error('Unknown type: ' + type);
             }
          });
@@ -65,6 +128,10 @@ define(function(require) {
       this.parameters.push({ name: '...' });
    };
 
+   /*
+    * The function `fun` takes as input the processed actuals list and needs
+    * to return the new value for the parameter.
+    */
    Resolver.prototype.addDefault = function(param, fun) {
       if (!this.has(param)) {
          throw new Error('Cannot add default for unknown parameter: ' + param);
@@ -73,6 +140,10 @@ define(function(require) {
       return this;
    };
 
+   /*
+    * The function `fun` takes as input the value of the `parent`
+    * and is meant to return the value for the `dependent`.
+    */
    Resolver.prototype.addDependent = function(dependent, parent, fun) {
       if (!this.has(dependent)) {
          throw new Error('Cannot add dependency for unknown parameter: ' + dependent);
@@ -86,11 +157,19 @@ define(function(require) {
       return this;
    };
 
+   /*
+    * The function `fun` takes as input the processed actuals list and can make
+    * changes to it. Its return value is ignored.
+    */
    Resolver.prototype.addNormalize = function(fun) {
       this.rules.push({ type: 'normalize', fun: fun });
       return this;
    };
 
+   /*
+    * Determines if a given parameter has already been defined. Used by the other
+    * resolver methods to ensure the validity of the specification.
+    */
    Resolver.prototype.has = function(param) {
       var i;
 
@@ -102,6 +181,16 @@ define(function(require) {
       return false;
    };
 
+   /*
+    * When this method is called, it is provided an "actuals" list
+    * for the function call to be evaluated. This list is then compared to
+    * the specifications in the `parameters` property and an initial
+    * processing is performed (e.g. vector->number and other conversions).
+    * This produces a new list, which is then transformed using the `rules` one
+    * at a time in the order in which they were inserted in the array.
+    *
+    * Returns the resulting list of processed actuals values.
+    */
    Resolver.prototype.resolve = function(actuals) {
       // TODO
       // BIG TODO
