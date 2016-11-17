78dd92f93646ba8ddc1942c370e49807b4a6f16a
Haris Skiadas <cskiadas@gmail.com>
2016-11-17 16:48:28 -0500

    Add qnorm and refactor resolving. ref #28

--- packages/stats.js
+++ packages/stats.js
@@ -12,24 +12,17 @@ define(function(require) {
             mu: lst.get('mean'), sigma: lst.get('sd')
          }));
       }, function(resolver) {
-         resolver.addParameter('n', ['number', 'variable'], true)
-            .addParameter('mean', ['scalar'], false)
-            .addParameter('sd', ['scalar'], false)
-            .addDefault('mean', function() { return Base.Variable.scalar([0]); })
-            .addDefault('sd', function() { return Base.Variable.scalar([1]); });
+         resolver.addParameter('n', ['number', 'variable'], true);
+         addNormalDefaults(resolver);
       });
       addBuiltin('dnorm', function(lst) {
          return Value.wrap(Base.stats.dnorm(lst.get('x'), {
             mu: lst.get('mean'), sigma: lst.get('sd'), log: lst.get('log')
          }));
       }, function(resolver) {
-         resolver.addParameter('x', ['scalar'], true)
-            .addParameter('mean', ['scalar'], false)
-            .addParameter('sd', ['scalar'], false)
-            .addParameter('log', ['boolean'], false)
-            .addDefault('mean', function() { return Base.Variable.scalar([0]); })
-            .addDefault('sd', function() { return Base.Variable.scalar([1]); })
-            .addDefault('log', function() { return false; });
+         resolver.addParameter('x', ['scalar'], true);
+         addNormalDefaults(resolver);
+         addLog(resolver);
       });
       addBuiltin('pnorm', function(lst) {
          return Value.wrap(Base.stats.pnorm(lst.get('x'), {
@@ -37,19 +30,40 @@ define(function(require) {
             log: lst.get('log'), lowerTail: lst.get('lower.tail')
          }));
       }, function(resolver) {
-         resolver.addParameter('x', ['scalar'], true)
-            .addParameter('mean', ['scalar'], false)
-            .addParameter('sd', ['scalar'], false)
-            .addParameter('log', ['boolean'], false)
-            .addParameter('log.p', ['boolean'], false)
-            .addParameter('lower.tail', ['boolean'], false)
-            .addDefault('mean', function() { return Base.Variable.scalar([0]); })
-            .addDefault('sd', function() { return Base.Variable.scalar([1]); })
-            .addDependent('log', 'log.p', function(log) { return log; })
-            .addDefault('log', function() { return false; })
-            .addDefault('lower.tail', function() { return true; });
+         resolver.addParameter('x', ['scalar'], true);
+         addNormalDefaults(resolver);
+         addLog(resolver);
+         addLogLowerTail(resolver);
+      });
+      addBuiltin('qnorm', function(lst) {
+         return Value.wrap(Base.stats.qnorm(lst.get('p'), {
+            mu: lst.get('mean'), sigma: lst.get('sd'),
+            log: lst.get('log'), lowerTail: lst.get('lower.tail')
+         }));
+      }, function(resolver) {
+         resolver.addParameter('p', ['scalar'], true);
+         addNormalDefaults(resolver);
+         addLog(resolver);
+         addLogLowerTail(resolver);
       });
    };
+
+   function addNormalDefaults(resolver) {
+      resolver.addParameter('mean', ['scalar'], false)
+         .addParameter('sd', ['scalar'], false)
+         .addDefault('mean', function() { return Base.Variable.scalar([0]); })
+         .addDefault('sd', function() { return Base.Variable.scalar([1]); });
+   }
+   function addLog(resolver) {
+      resolver.addParameter('log', ['boolean'], false)
+         .addParameter('log.p', ['boolean'], false)
+         .addDependent('log', 'log.p', function(log) { return log; })
+         .addDefault('log', function() { return false; });
+   }
+   function addLogLowerTail(resolver) {
+      resolver.addParameter('lower.tail', ['boolean'], false)
+         .addDefault('lower.tail', function() { return true; });
+   }
 });
 
 }(typeof define === 'function' && define.amd ? define : function(factory) {
